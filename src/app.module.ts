0faa97fc3f4788598e289de09178610d7febdfe6
MaStEr <masternonnolnw@gmail.com>
2022-02-13 23:07:08 +0700

    env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,13 +8,21 @@ import { TripModule } from './trip/trip.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.local', '.env.production'],
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '127.0.0.1',
+      host: process.env.HOST,
       port: 3306,
-      username: 'root',
-      password: '4913',
-      database: 'trip',
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      // host: 'us-cdbr-east-05.cleardb.net',
+      // port: 3306,
+      // username: 'b8b0329c36c3b8',
+      // password: '99fb65aa',
+      // database: 'heroku_43cbd1ee81dba97',
       entities: [Trip],
       synchronize: true,
     }),
