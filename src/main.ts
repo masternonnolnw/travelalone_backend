cd34bf860d59e42ba81237ee7ee89c1188c85a8e
MaStEr <masternonnolnw@gmail.com>
2022-02-14 14:48:05 +0700

    set config

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  await app.listen(8000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
